8c976a818000bf54cd832d2d3f719798ae4eee69
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-22 08:07:16 +0000

    Deploy Michael-F-Bryan/gcode-rs to github.com/Michael-F-Bryan/gcode-rs.git:gh-pages

---
+++ uom/si/marker/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"trait":[["AngleKind","AngleKind is a `Kind` for separating angular quantities from their indentically dimensioned non-angular quantity counterparts. Conversions to and from `AngleKind` quantities is supported through implementations of the `From` trait."],["TemperatureKind","Kind of thermodynamic temperature."]]});
